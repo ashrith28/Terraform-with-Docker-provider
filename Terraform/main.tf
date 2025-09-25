743729dc5eeaf3a56c91042c426e8b20fff2eb58
Kalasam Ashrith <ashu.ashrith28@gmail.com>
2025-09-25 11:44:36 +0000

    Terraform file added

---
+++ Terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull the nginx image
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Run a container from the image
+resource "docker_container" "nginx" {
+  name  = "nginx_container"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
